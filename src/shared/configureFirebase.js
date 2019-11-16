a1e69baef2e0109d562c3d648f969e013195ee17
monika aggarwal <monika.aggarwal@proptiger.com>
2019-11-16 10:37:43 +0530

    changes

--- src/shared/configureFirebase.js
+++ src/shared/configureFirebase.js
@@ -4,12 +4,12 @@ import 'firebase/firestore'
 
 // TODO: Replace the following with your app's Firebase project configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyDVJLLBWIhjVU5ogm2GtyALnADZdJ_z_lU",
-  authDomain: "tasklist-2be89.firebaseapp.com",
-  databaseURL: "https://tasklist-2be89.firebaseio.com",
-  projectId: "tasklist-2be89",
-  storageBucket: "tasklist-2be89.appspot.com",
-  messagingSenderId: "493798251149"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
 // Initialize Firebase
