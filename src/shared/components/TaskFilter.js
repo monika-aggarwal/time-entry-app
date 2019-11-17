0134c0090213eaa6d7e9a50725bbb65abbec8414
monika aggarwal <monika.aggarwal@proptiger.com>
2019-11-17 16:31:57 +0530

    done

--- src/shared/components/TaskFilter.js
+++ src/shared/components/TaskFilter.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import DatePicker from 'react-datetime'
-import { loginContainer, loginBtn } from 'src/shared/components/style'
+import { loginContainer, loginBtn, errorStyle } from 'src/shared/components/style'
 
 const getStartDate = () => {
   let startDate = new Date()
@@ -11,36 +11,49 @@ const getStartDate = () => {
 
 const ReportFilter = ({ submit }) => {
   const [{
-    startDate, endDate
+    startDate, endDate, errorMessage
   }, setState] = useState({
     startDate: getStartDate(),
     endDate: new Date()
   })
 
+  const setData = (field, value) => {
+    setState(prev => ({ ...prev, [field]: value }))
+  }
+
   const handleStartDateChange = (date) => {
-    setState(prev => ({ ...prev, startDate: date }))
+    setData('startDate', date)
   }
 
   const handleEndDateChange = (date) => {
-    if (date > startDate) {
-      setState(prev => ({ ...prev, endDate: date }))
+    setData('endDate', date)
+  }
+
+  const onClick = () => {
+    if (endDate < startDate) {
+      return setData('errorMessage', 'Please select valid date range')
     }
+    setData('errorMessage', '')
+    submit(startDate, endDate)
   }
 
   return (
     <div css={loginContainer}>
       <h2>View Your previous tasks</h2>
-        <label>Start Date</label>
-        <DatePicker
-          inputProps={{ placeholder: '' }}
-          onChange={(date) => handleStartDateChange(date.valueOf())}
-        />
-        <label>End Date</label>
-        <DatePicker
-          inputProps={{ placeholder: '' }}
-          onChange={(date) => handleEndDateChange(date.valueOf())}
-        />
-        <button css={loginBtn} onClick={() => submit(startDate, endDate)}>Show Tasks</button>
+      <label>Start Date</label>
+      <DatePicker
+        defaultValue={startDate}
+        onChange={(date) => handleStartDateChange(date.valueOf())}
+      />
+      <label>End Date</label>
+      <DatePicker
+        defaultValue={endDate}
+        onChange={(date) => handleEndDateChange(date.valueOf())}
+      />
+      <button css={loginBtn} onClick={() => onClick()}>Show Tasks</button>
+      {
+        errorMessage && <span css={errorStyle}>{errorMessage}</span>
+      }
     </div>
   )
 }
